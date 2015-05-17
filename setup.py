b2a22db09d8432b1a1d591b20dd343d42607b6f4
Nicholas Bollweg <nick.bollweg@gmail.com>
2015-05-17 15:24:07 -0400

    formatting

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ except:
     import pip
     import importlib
 
-    pip.main(['install', 'jupyter-pip'])
-    cmdclass = importlib.import_module('jupyterpip').cmdclass
+    pip.main(["install", "jupyter-pip"])
+    cmdclass = importlib.import_module("jupyterpip").cmdclass
 
 
 with open("setup.json") as f:
@@ -23,7 +23,11 @@ with open("README.rst") as f:
     )
 
 setup_data.update(
-    cmdclass=cmdclass(path="{0}/static/{0}".format(setup_data["py_modules"][0]))
+    cmdclass=cmdclass(
+        path="{0}/static/{0}".format(
+            setup_data["py_modules"][0]
+        )
+    )
 )
 
 setup(**setup_data)
